bb7844a55e20bc54dbd69713c1efa5d12ba39f77
Miirshe <miirshe@gmail.com>
2023-06-29 17:47:34 +0300

    added delete operation

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,17 +1,18 @@
 import { TiDocumentDelete } from "react-icons/ti";
 import { LuFileEdit } from "react-icons/lu";
 import { Link } from "react-router-dom";
-import { useGetNotesQuery } from "../store/api/NoteSlice";
+import { useDeleteNotesMutation, useGetNotesQuery } from "../store/api/NoteSlice";
 import { useGetUserQuery } from "../store/api/UserSlice";
 const Notes = () => {
+  const [deleteNotes] = useDeleteNotesMutation();
   const { data: notes = [] } = useGetNotesQuery();
   const { data: users = {} } = useGetUserQuery();
 
   console.log(notes);
   console.log("users", users);
-  // const handleDlete = ()=>{
-
-  // }
+  const handleDlete = (id)=>{
+    deleteNotes(id)
+  }
   return (
     <div className="mt-10 p-5 lg:w-10/12 shadow-inner  lg:mx-auto ">
       <p className="text-3xl p-3 my-3 text-slate-600">List notes </p>
@@ -27,7 +28,7 @@ const Notes = () => {
                   <p className="text-base">{note.content}</p>
                   <div className="flex flex-row border-t-2 p-2">
                     <button><Link to={`/EditNote/${note.id}`}><LuFileEdit size={25} className="text-green-500" /></Link></button>
-                    <button><TiDocumentDelete size={25} className="text-red-500" /></button>
+                    <button onClick={()=>handleDlete(note.id)}><TiDocumentDelete size={25} className="text-red-500" /></button>
                   </div>
                 </div>
               )
